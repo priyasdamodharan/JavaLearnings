e4e704516357dd66602451bbd650205499f7fadf
Priyadharshini Damodharan <77445249+priyasdamodharan@users.noreply.github.com>
2021-01-16 19:43:13 +0530

    Simple Banking Application
    A banking application that can perform the following functionalities : 
    
    A. Check your balance
    B. Make a deposit
    C. Make a withdrawal
    D. View previous transaction
    E. Calculate interest
    F. Exit

---
+++ BankingApplication/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Account tim = new Account("Tim Statler","A00001");
+        tim.showMenu();
+    }
+}
