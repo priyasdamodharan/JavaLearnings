3325a268cae5a97eea1d7936548d872ceacad06d
Priyadharshini Damodharan <77445249+priyasdamodharan@users.noreply.github.com>
2021-04-06 08:51:13 +0530

    Challenge

---
+++ AbstractChallenge/NodeList.java
@@ -0,0 +1,7 @@
+package com.company;
+public interface NodeList {
+    ListItem getRoot();
+    boolean addItem(ListItem item);
+    boolean removeItem(ListItem item);
+    void traverse(ListItem root);
+}
